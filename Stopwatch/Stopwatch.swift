93b93edd78403c069f14b71c2e9ceb50c070e809
funday991 <funday991@gmail.com>
2019-03-07 16:06:36 +0500

    initial commit

---
+++ Stopwatch/Stopwatch.swift
@@ -0,0 +1,80 @@
+//
+//  Stopwatch.swift
+//  Stopwatch
+//
+//  Created by Yury on 06/03/2019.
+//  Copyright © 2019 napoleon. All rights reserved.
+//
+
+import Foundation
+
+
+class Stopwatch {
+    
+    enum State { case paused, running }
+    
+    var state: State
+    var centisecondsCounter: Int
+    var updateTimeLabelCallback: () -> Void
+    
+    var formattedTime: String {
+        let centiseconds = centisecondsCounter % 100
+        let seconds = centisecondsCounter / 100 % 60
+        let minutes = centisecondsCounter / 6000 % 60
+        let hours = centisecondsCounter / 360000
+        
+        return String(format: "%0.2d:%0.2d:%0.2d.%0.2d", hours, minutes, seconds, centiseconds)
+    }
+    
+    var timer: Timer?
+    
+    
+    init(state: State, currentCentiseconds: Int, callbackOnFire: @escaping () -> Void) {
+        self.state = state
+        self.centisecondsCounter = currentCentiseconds
+        self.updateTimeLabelCallback = callbackOnFire
+    }
+    
+    
+    func toggle() {
+        switch state {
+        case .paused:
+            state = .running
+            run()
+        case .running:
+            state = .paused
+            pause()
+        }
+    }
+    
+    
+    func run() {
+        timer = Timer.scheduledTimer(
+            timeInterval: 0.01,
+            target: self,
+            selector: #selector(updateTime),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    
+    @objc func updateTime() {
+        centisecondsCounter += 1
+        
+        updateTimeLabelCallback()
+    }
+    
+    
+    func pause() {
+        timer?.invalidate()
+    }
+    
+    
+    func reset() {
+        centisecondsCounter = 0
+        
+        updateTimeLabelCallback()
+    }
+    
+}
--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toggleButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var stopwatch: Stopwatch?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        NotificationCenter.default.addObserver(
+            forName: UIApplication.willEnterForegroundNotification,
+            object: nil,
+            queue: nil
+        ) { (notification) in
+            if self.stopwatch?.state == .running {
+                self.stopwatch?.centisecondsCounter += UserDefaults.standard.integer(forKey: "backgroundStopwatchValue")
+            }
+        }
+        
+        timeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 40, weight: UIFont.Weight.thin)
+        
+        stopwatch = Stopwatch(state: .paused, currentCentiseconds: 0, callbackOnFire: updateTimeLabel)
     }
-
-
+    
+    @IBAction func toggleButtonTapped(_ sender: UIButton) {
+        stopwatch?.toggle()
+        
+        switch stopwatch!.state {
+        case .running:
+            toggleButton.setTitleColor(.red, for: .normal)
+            toggleButton.setTitle("Pause", for: .normal)
+            resetButton.isEnabled = false
+        case .paused:
+            toggleButton.setTitleColor(.green, for: .normal)
+            toggleButton.setTitle("Run", for: .normal)
+            resetButton.isEnabled = true
+        }
+    }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        stopwatch?.reset()
+    }
+    
+    
+    func updateTimeLabel() {
+        timeLabel.text = stopwatch?.formattedTime
+    }
+    
 }
 
